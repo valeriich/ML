64d2755e3e09d3f424ed6277cc14f376fcbba69a
valeriich <iixmcmlxxiii@gmail.com>
2018-11-13 09:36:23 +0200

    hello2

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def norm_df(df):
-    result = df.copy()
 
+def norm_df(df):
+    result = df.cpopy()
     for feature in df.columns:
         result[feature] = norm_arr(result[feature])
 
     return result
+
 
 def norm_arr(arr):
     mean = arr.mean()
@@ -24,6 +25,8 @@ df1 = df[df['Age'].notnull()]
 print(norm_arr(df1['Age']))
 df1['Age'].iloc[0:3] = 1000
 print(norm_arr(df1['Age']))
-df1.Age.quantile(0.025)
-df1.Age.quantile(0.975)
-
+selection = (df1['Age'] < df1.Age.quantile(0.975)) & (df1['Age'] > df1.Age.quantile(0.025))
+print(selection)
+selected = df1[selection]
+print(selected['Age'])
+print(norm_arr(selected['Age']))
