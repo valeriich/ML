eb44883b041fee9d789b8ab7dc0bc044bd0153f5
valeriich <iixmcmlxxiii@gmail.com>
2018-11-13 09:46:08 +0200

    hello2

--- test2.py
+++ test2.py
@@ -25,6 +25,7 @@ df1 = df[df['Age'].notnull()]
 print(norm_arr(df1['Age']))
 df1['Age'].iloc[0:3] = 1000
 print(norm_arr(df1['Age']))
+# удалить строки с крайними 2,5% значениями в столбце 'Age'
 selection = (df1['Age'] < df1.Age.quantile(0.975)) & (df1['Age'] > df1.Age.quantile(0.025))
 print(selection)
 selected = df1[selection]
