7b37e2ca5389a2ac00f9d16adc77652b2099eb53
valeriich <iixmcmlxxiii@gmail.com>
2018-11-23 15:15:45 +0200

    homework

--- adult.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-data = pd.read_csv('adultdata.csv', header=None, sep=', ')
-data.columns = [
-    "Age", "WorkClass", "fnlwgt", "Education", "EducationNum",
-    "MaritalStatus", "Occupation", "Relationship", "Race", "Gender",
-    "CapitalGain", "CapitalLoss", "HoursPerWeek", "NativeCountry", "Income"
-]
-M = {'Married-civ-spouse', 'Married-spouse-absent', 'Married-AF-spouse'}
